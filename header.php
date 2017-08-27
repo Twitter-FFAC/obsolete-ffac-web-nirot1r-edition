cf7675f8d488368711f3ff512e17bca8a7f4173b
小野咲みなみ <mnmonzk@gmail.com>
2017-08-28 00:24:47 +0900

    index.phpを分離

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="/bootstrap.min.css" media="screen">
+        <link rel="stylesheet" href="/style.css" media="screen">
+        <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>        
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Fight for artistic creativity</title>
+    </head>
+    <body>
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <link rel="stylesheet" href="/bootstrap.min.css" media="screen">
-        <link rel="stylesheet" href="/style.css" media="screen">
-        <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>        
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>Fight for artistic creativity</title>
-    </head>
-    <body>
+<?php include "header.php"; ?>
         <div class="container shadow">
 
             <h1 class="page-header">Fight for artistic creativity</h1>
@@ -64,8 +54,4 @@
             </div>
             
         </div>
-
-            
-    </body>
-    
-</html>
+<?php include "footer.php"; ?>
