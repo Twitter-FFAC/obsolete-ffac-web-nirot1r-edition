fede6899ad75af5b8b118b7bcf9e58c1b9223c9f
小野咲みなみ <mnmonzk@gmail.com>
2017-08-27 23:26:40 +0900

    フッターにリポジトリ情報を追記

--- index.php
+++ index.php
@@ -79,7 +79,8 @@
             </div>
 
             <div class="profile shadow">
-                Project with <span class="text-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>&nbsp;<a href="https://github.com/acid-chicken/fight-for-artistic-creativity"><i class="fa fa-github" aria-hidden="true"></i>&nbsp;Fight for artistic creativity by acid-chicken</a>
+                Project with <span class="text-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>&nbsp;<a href="https://github.com/acid-chicken/fight-for-artistic-creativity"><i class="fa fa-github" aria-hidden="true"></i>&nbsp;Fight for artistic creativity by acid-chicken</a><br>
+                このWEBサイトは、GitHubにてバージョン管理しています。リポジトリは<a href="https://github.com/mnmonzk/ffac-web">こちら</a>です。
             </div>
             
         </div>
