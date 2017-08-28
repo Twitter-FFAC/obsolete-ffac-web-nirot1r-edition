f359315055f1d63d7d7c912aa92eda63ae769d74
小野咲みなみ <mnmonzk@gmail.com>
2017-08-28 10:29:04 +0900

    共有リンクを追加

--- header.php
+++ header.php
@@ -27,4 +27,7 @@
 ?>
         <h1 class="page-header">Fight for artistic creativity</h1>
         <p class="lead">Twitterをディストピアにしないために、我々ができること。</p>
+        <div class="text-right">
+            プロジェクトを共有 : <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        </div>
     </div>
