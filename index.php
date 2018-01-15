b7966ef3c48f404f593a1dbd84206143712b2913
Tomotada Saiugusa <nirot1r@g-second.net>
2018-01-15 22:55:42 +0900

    ✏ ベースデザインの作成

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ffac-web</title>
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="header-title">
+                Fight for artistic creativity / 自分を守る、みんなで守る。
+            </div>
+            <div class="header-subtitle">
+                不適切な内容を投稿しているユーザから身を守る方法を提供しています。
+            </div>
+        </div>
+        <div class="container">
+            <div class="column">
+                <div class="card">
+                    <div class="card-icon"><i class="fa fa-user-circle-o" aria-hidden="true"></i></div>
+                    <div class="card-header">自分で守る</div>
+                    <div class="card-content">
+                        簡単な操作でリストアップされているアカウントを<br />
+                        スパム報告・ブロックすることができます。<br />
+                        <a class="btn" href="" target="blank">この方法で守る</a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="card-icon"><i class="fa fa-github-alt" aria-hidden="true"></i></div>
+                    <div class="card-header">みんなで守る</div>
+                    <div class="card-content">
+                        リストアップされているアカウントは、<br />
+                        全てGitHub上でバージョン管理されています。<br />
+                        <a class="btn" href="https://github.com/Twitter-FFAC/fight-for-artistic-creativity/issues/6" target="blank">不審なユーザを見つけたら、ここから投稿しましょう。</a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="card-icon"><i class="fa fa-server" aria-hidden="true"></i></div>
+                    <div class="card-header">ホストして守る</div>
+                    <div class="card-content">
+                        このWEBサイト・システムは、<br />
+                        GitHub上でバージョン管理されています。<br />
+                        あなたがWEBサーバをお持ちであれば、<br />
+                        かんたんな操作でこのWEBサイトを構築することができます。<br />
+                        <a class="btn" href="" target="blank">詳しく</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
