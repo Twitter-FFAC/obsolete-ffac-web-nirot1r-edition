ce94b2408893377832aaa1bba0870351acf6ab14
Tomotada Saiugusa <nirot1r@g-second.net>
2018-01-17 22:36:12 +0900

    🔥 過去データの削除

--- old/footer.php
+++
@@ -1,6 +0,0 @@
-
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-        <script src="/bootstrap.min.js"></script>
-    </body>
-    
-</html>
